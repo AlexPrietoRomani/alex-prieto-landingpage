e046a8e4fa72ac35486bad754f9244082a619c85
Alex Prieto <alexprieto1997@gmail.com>
2025-12-09 12:23:15 -0500

    Update site URL in Astro config
    Changed the site URL in astro.config.mjs to use lowercase letters for consistency and correctness.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://AlexPrietoRomani.github.io',
+  site: 'https://alexprietoromani.github.io',
   base: '/',
   integrations: [react()]
 });
