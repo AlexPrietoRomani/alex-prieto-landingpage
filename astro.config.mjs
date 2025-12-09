6181d67fd4e8261bf9259b2c938f5a650839472d
Alex Prieto <alexprieto1997@gmail.com>
2025-12-09 11:56:26 -0500

    Add site URL and base path to Astro config
    Configured the 'site' and 'base' properties in astro.config.mjs to specify the deployment URL and base path for the project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://AlexPrietoRomani.github.io',
+  base: '/',
   integrations: [react()]
 });
